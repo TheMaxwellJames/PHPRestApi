49cf6d7fd5b2cdfa25b04caedce46d3bec52e50f
TheMaxwellJames <maxichanj@gmail.com>
2023-12-12 02:49:33 +0100

    create Api to insert data into database

--- customers/function.php
+++ customers/function.php
@@ -2,6 +2,69 @@
 
 require_once '../inc/dbcon.php';
 
+function error422($message)
+{
+    $data = [
+        'status' => 422,
+        'message' => $message,
+
+    ];
+    header("HTTP/1.0 422 Unprocessable Entity");
+    echo json_encode($data);
+    exit();
+}
+
+function  storeCustomer($customerInput)
+{
+    global  $conn;
+
+    $name = mysqli_real_escape_string($conn, $customerInput['name']);
+    $email = mysqli_real_escape_string($conn, $customerInput['email']);
+    $phone = mysqli_real_escape_string($conn, $customerInput['phone']);
+
+    if(empty(trim($name)))
+    {
+        return error422('Enter your name');
+    }
+    elseif(empty(trim($email))) 
+    {
+        return error422('Enter your email');
+    }
+    elseif(empty(trim($phone))) 
+    {
+        return error422('Enter your phone');
+
+    }
+    else 
+    {
+        $query = "INSERT INTO customers (name,email,phone) VALUES ('$name','$email','$phone')";
+        $result = mysqli_query($conn, $query);
+
+        if($result)
+        {
+            $data = [
+                'status' => 201,
+                'message' => 'Customer Created Successfully',
+        
+            ];
+            header("HTTP/1.0 201 Created");
+            echo json_encode($data);
+        }
+        else 
+        {
+
+            $data = [
+                'status' => 500,
+                'message' => 'Internal Server Error',
+        
+            ];
+            header("HTTP/1.0 500 Internal Server Error");
+            echo json_encode($data);
+
+        }
+    }
+}
+
 function getCustomerList()
 {
 
@@ -51,3 +114,5 @@ function getCustomerList()
   }
 
 }
+
+?>
